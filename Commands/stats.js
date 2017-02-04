8f3bb5aa7a95f7db2cdcb234302fa92f1d5b000a
Jack <epicawesomewolfgaming@gmail.com>
2017-02-04 15:44:47 -0500

    i was exposed

--- Commands/stats.js
+++ Commands/stats.js
@@ -10,7 +10,7 @@ module.exports = class Stats {
   async run(message, args) {
     const embed = new Discord.RichEmbed();
     embed.setColor(0xffffff);
-    embed.setTitle("Google Home's Current Stats")
+    embed.setTitle("Hammer's Current Stats")
     embed.setURL("https://google.com")
     embed.setDescription(`__**Bot Stats**__\n\n**Guild Count**: ${this.client.guilds.size} guilds\n**User Count**: ${this.client.users.size} users\n**Channel Count**: ${this.client.channels.size} channels\n**Uptime**: ${Math.round(this.client.uptime / (1000 * 60 * 60))} hours, ${Math.round(this.client.uptime / (1000 * 60)) % 60} minutes, and ${Math.round(this.client.uptime / 1000) % 60} seconds\n\n__**Other Stats**__\n\n**Memory Usage**: ${(process.memoryUsage().rss / 1024 / 1024).toString().split(".")[0]} MiB\n**Owners**: Jack#0305, DoctorDarkMagic ツ#3790`)
     embed.setTimestamp();
