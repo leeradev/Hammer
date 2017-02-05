c3bed4446765d89f5f0175acd7a78dbcbc9e5dd1
DoctorDarkMagic <jacobpbond@hotmail.co.uk>
2017-02-05 01:31:30 +0000

    Server ID

--- Commands/conf.js
+++ Commands/conf.js
@@ -13,7 +13,7 @@ module.exports = class Info {
             this.client.sql.open('./hammer.sqlite').then(() => {
               this.client.sql.get(`SELECT * FROM servers WHERE serverID ='${message.guild.id}'`).then(row => {
                 if(!row) return message.reply('No data found for this server.');
-                message.reply(`__**Row Data**__\n\n**Access Role:** ${row.accessRole}\n**Logging Channel:** ${row.loggingCh}\n**Anti-Invites:** ${row.antiAdv}`);
+                message.reply(`__**Row Data**__\n\n**Server ID:** ${row.serverID}\n**Access Role:** ${row.accessRole}\n**Logging Channel:** ${row.loggingCh}\n**Anti-Invites:** ${row.antiAdv}`);
             })
           })
         }
