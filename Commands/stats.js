dd9f274c84a679477010c7dbb257187e120db16f
Jack <epicawesomewolfgaming@gmail.com>
2017-02-18 10:43:11 -0500

    woo new logo xD

--- Commands/stats.js
+++ Commands/stats.js
@@ -14,7 +14,7 @@ module.exports = class Stats {
     embed.setURL("https://google.com")
     embed.setDescription(`__**Bot Stats**__\n\n**Guild Count**: ${this.client.guilds.size} guilds\n**User Count**: ${this.client.users.size} users\n**Channel Count**: ${this.client.channels.size} channels\n**Uptime**: ${Math.round(this.client.uptime / (1000 * 60 * 60))} hours, ${Math.round(this.client.uptime / (1000 * 60)) % 60} minutes, and ${Math.round(this.client.uptime / 1000) % 60} seconds\n\n__**Other Stats**__\n\n**Memory Usage**: ${(process.memoryUsage().rss / 1024 / 1024).toString().split(".")[0]} MiB\n**Owners**: Jack#0305, DoctorDarkMagic ツ#3790`)
     embed.setTimestamp();
-    embed.setFooter("Generated at ", `${this.client.user.avatarURL}`)
+    embed.setFooter("Generated at ", `${this.client.user.avatarURL.replace('.jpg', '.png')}`)
     message.channel.sendEmbed(embed)
   }
 }
