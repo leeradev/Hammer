403d6e683da6fa564bfacfec2d60ef7d410995ac
DoctorDarkMagic <jacobpbond@hotmail.co.uk>
2017-02-18 15:28:40 +0000

    Database

--- hammer.js
+++ hammer.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js')
-const paste = require('better-pastebin')
+const paste = require('better-pastebin');
 
-const events = require('./Modules/Events.js')
+const events = require('./Modules/Events.js');
+const data   = require('./Modules/Data.js');
 
 const client = new class Client extends Discord.Client {
     constructor() {
         super({
             fetchAllMembers: true
         });
-        this.events = new events(this)
-        this.commands = new Map()
-        this.sql = require('sqlite')
+
+        
+        this.events   = new events(this)
+        this.data     = new data(this);
+        this.commands = new Map();
+        this.sql      = require('sqlite');
 
 
         this.logger = new Discord.WebhookClient('277540915504087040', '0rtOAJT7wklRQMP66jgEnCqXYUEjgjAmofzZyNUPbUpl17yM51o_zjAv2cNHA_3bYB-t');
